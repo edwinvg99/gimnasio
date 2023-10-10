e037523d2280984c2514af1dfe516d0c97bc86bf
edwinvg99 <velasquezgiraldoedwin@gmail.com>
2023-10-10 17:56:25 -0500

    actualizacion con bloqueo y desbloqueo de usuarios

--- gimnasioweb/src/components/UserList.js
+++ gimnasioweb/src/components/UserList.js
@@ -12,7 +12,10 @@ const UserList = () => {
         const fetchUsers = async () => {
             try {
                 const usersCollection = await firebase.db.collection('users').get();
-                const users = usersCollection.docs.map((doc) => doc.data());
+                const users = usersCollection.docs.map((doc) => ({
+                    ...doc.data(),
+                    isLocked: false, // Inicialmente, ningún usuario está bloqueado
+                }));
                 setUserList(users);
             } catch (error) {
                 console.error('Error al obtener la lista de usuarios:', error);
@@ -22,32 +25,57 @@ const UserList = () => {
         fetchUsers();
     }, [firebase]);
 
-
     return (
         <div id="MainList" className="bg-white p-4">
-
             <h1 id="title" className="text-4xl font-bold text-gray-800 mb-4 text-center">Lista de Usuarios Registrados</h1>
-
             <div className="bg-red-000 w-full flex flex-wrap ">
-
                 <ul id="luMain" className="w-full flex flex-wrap ">
-                    {userList.map((user, index) => (
-                        <li id="liMain" className="bg-white rounded-lg shadow-md p-4 mb-4 w-1/5 h-auto m-5" key={index}>
+                    {userList.map((user) => (
+                        <li
+                            id="liMain"
+                            className="bg-white rounded-lg shadow-md p-4 mb-4 w-1/5 h-auto m-5"
+                            key={user.id}
+                            style={{
+                                backgroundColor: user.isLocked ? 'gray' : 'white',
+                                color: user.isLocked ? 'white' : 'black',
+                            }}
+                            
+                        >
                             <strong className="text-lg text-violet-800">Nombre: {user.user}</strong>
                             <p>Cédula: {user.id}</p>
                             <p>Usuario: {user.userName}</p>
                             <p>Email: {user.email}</p>
-                            <button onClick={() => { navigate('/asignar-entrenamiento') }} className="bg-violet-800 text-white px-4 py-2 rounded mt-4 hover:bg-violet-600 hover:scale-105 transition duration-300">
-                                Asignar rutina
+
+                            <button
+                                onClick={() => {
+                                    user.isLocked = !user.isLocked; // Cambiar el estado de bloqueo del usuario
+                                    
+                                    setUserList([...userList]); // Actualizar la lista de usuarios
+                                }}
+                                className=" m-1 bg-violet-800 text-white px-4 py-2 rounded mt-4 hover:bg-violet-600 hover:scale-105 transition duration-300"
+                            >
+                                {user.isLocked ? 'Desbloquear' : 'Bloquear'}
                             </button>
 
-                           
+                            <button
+                                onClick={() => {
+                                    navigate('/asignar-entrenamiento');
+                                }}
+                                className="m-1 bg-violet-800 text-white px-4 py-2 rounded mt-4 hover:bg-violet-600 hover:scale-105 transition duration-300"
+                            >
+                                Asignar rutina
+                            </button>
                         </li>
                     ))}
                 </ul>
-                
             </div>
-            <button id="ButtonExit" onClick={() => { navigate('/User') }} className="bg-violet-800 text-white px-4 py-2 rounded mt-4 hover:bg-violet-600 hover:scale-105 transition duration-300">
+            <button
+                id="ButtonExit"
+                onClick={() => {
+                    navigate('/User');
+                }}
+                className="bg-violet-800 text-white px-4 py-2 rounded mt-4 hover:bg-violet-600 hover:scale-105 transition duration-300"
+            >
                 Volver
             </button>
         </div>
